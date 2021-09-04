a630c5dbcd15de2801932124a73252e180294349
shahzaibansari123 <ansarishahzaib567@gmail.com>
2021-09-04 05:11:43 +0500

    work complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Count = () => {
+  const [isBright, setIsDark] = React.useState(true);
+  const display = document.querySelector('body');
+  display.className = isBright ? "Bright" : "Dark";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const theme = () => {
+    setIsDark(!isBright);
+  }
+
+  const [temp, setIsTempCount] = React.useState(70);
+
+  const countIncrease = () => {
+    setIsTempCount(temp + 1);
+  }
+  const countDecrease = () => {
+    setIsTempCount(temp - 1);
+  }
+
+  return <div>
+    <div class="heading">
+      Temperature Count
+      <label class="switch">
+        <input type="checkbox"></input>
+        <span onClick={theme} class="slider round"> </span>
+      </label>
+      <br></br>
+      </div>
+      <div class="btns">
+      <button class="count1" onClick={countIncrease}>+</button>
+      <div>{temp}<span class="degree">°C</span></div>
+      <button class="count2" onClick={countDecrease}>-</button>
+    </div>
+    </div>
+    
+}
+
+ReactDOM.render(<Count />, document.querySelector('#root'))
